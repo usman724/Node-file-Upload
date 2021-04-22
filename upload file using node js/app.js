ef8706ba04af309ef7b6715bfbcc09a9ac85bdb1
usman724 <40547670+usman724@users.noreply.github.com>
2021-04-22 15:55:41 +0500

    new commit
    new add fun

--- upload file using node js/app.js
+++ upload file using node js/app.js
@@ -3,8 +3,11 @@
 const express = require('express');
 const path = require('path');
 const multer = require('multer');
+const sharp = require('sharp')
+const fs = require('fs');
 var app = express();
-app.set('view engine','ejs'); 
+app.set('view engine', 'ejs');
+
 
 
 
@@ -13,24 +16,51 @@ var storage = multer.diskStorage({
     cb(null, './Views/uploads')
   },
   filename: function (req, file, cb) {
-    cb(null, file.fieldname + '-' + Date.now()+path.extname(file.originalname))
+    cb(null, file.fieldname + '-' + Date.now() + path.extname(file.originalname))
   }
 })
 
 
 
 var upload = multer({
-   storage:storage
+  storage: storage
 }).single('file');
-app.post('/UploadFile',upload,(req,res,next)=>{
-       res.send('File:'+req.file.fieldname);
+
+
+app.post('/UploadFile', upload, async (req, res, next) => {
+  console.log(req.file);
+
+  try {
+
+    await sharp(req.file.path).resize(300, 200).png().toFile('Views/uploads/' + req.file.originalname);
+
+    fs.unlink(req.file.path, (err) => {
+
+      if (err) {
+        console.error(err)
+        return
+      }
+      res.send('File:' + req.file.fieldname);
+      //file removed
+    })
+
+
+  } catch (error) {
+    console.log(error);
+  }
+
+
+  //  res.send('File:'+req.file.fieldname);
 })
-app.get('/form',(req,res)=>{
-      res.render("form",{
-      "data" : "2e312"
-     });
+
+
+app.get('/form', (req, res) => {
+  res.render("form", {
+    "data": "2e312"
+  });
 
 });
-app.listen('3000',()=>{
+app.listen('3000', () => {
   console.log('Connect and Running On Port 3000');
 });
+
